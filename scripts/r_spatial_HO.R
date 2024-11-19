27d388722d8bba2174f4bcc7593df746a093fb32
Han Olff <h.olff@rug.nl>
2024-11-19 01:08:29 +0100

    script update

--- scripts/r_spatial_HO.R
+++ scripts/r_spatial_HO.R
@@ -426,6 +426,7 @@ pointdata<-cbind(dist2river_points[,2],elevation_points[,2],
   as_tibble()
 pointdata
 pointdata<-pointdata[complete.cases(pointdata),]
+write_csv(pointdata,"pointdata.csv")
 
 # plot how woody cover is predicted by different variables
 # Create a correlation panel plot
